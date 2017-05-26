8d75ebeeb45c07dc2ce8fa88fe0540a0edecb17e
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-26 14:40:32 -0700

    Added Revis24 tweets

--- sampletweets.js
+++ sampletweets.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//example code for tweet
 {
   "text": "",
   "entities": {
@@ -10,11 +11,128 @@
     ]
   },
   "retweet_count": 0,
-  "favourites_count" 0,
-  "created_at": "Sun Apr 03 23:48:36 +0000 2011",
+  "favourites_count": 0,
+  "created_at": "Sat Apr 03 23:48:36 +0000 2011",
   "user": {
     "follower_count": 0,
     "screen_name": "",
-    "time_zone": "",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "Hey everyone @rsherman_25 wants to be a lockdown corner. He wants to be in the talks of a lockdown corner. So follow him so he can become 1!",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      }
+    ]
+  },
+  "retweet_count": 1324,
+  "favourites_count": 433,
+  "created_at": "Sat Feb 20 14:32:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
+  },
+},
+
+{
+  "text": "Look I just helped you get 6,000 followers homie!!! @rsherman_25",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      }
+    ]
+  },
+  "retweet_count": 1045,
+  "favourites_count": 620,
+  "created_at": "Sat Feb 20 14:50:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
+  },
+},
+
+{
+  "text": "That's like you saying your better then Deion Sanders & since he's not playing no more he's irrelevant for what he's done. @rsherman_25",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      }
+    ]
+  },
+  "retweet_count": 709,
+  "favourites_count": 271,
+  "created_at": "Sat Feb 20 14:29:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "I never seen a man before run his mouth so much like girl. This dude just steady putting my name in his mouth to get notoriety @rsherman_25",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      }
+    ]
+  },
+  "retweet_count": 6697,
+  "favourites_count": 3216,
+  "created_at": "Sat Feb 20 13:38:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@rsherman_25 I ain't your competition bro, @roddywhiteTV is!!!",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      },
+      {
+        "screen_name": "@roddywhiteTV",
+      }
+    ]
+  },
+  "retweet_count": 1218,
+  "favourites_count": 461,
+  "created_at": "Sat Apr 03 13:42:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@rsherman_25 child please, lol!!! Your still taking baby steps son. Don't get to a head of yourself.",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "@rsherman_25",
+      }
+    ]
+  },
+  "retweet_count": 785,
+  "favourites_count": 297,
+  "created_at": "Sat Apr 03 13:42:00 +0000 2013",
+  "user": {
+    "follower_count": 463110,
+    "screen_name": "@Revis24",
+    "time_zone": "Pacific",
   },
 }
