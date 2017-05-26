2bfb4e953fd130132fae49cac22e088697741400
DKosmos <Davidakosmos@gmail.com>
2017-05-26 14:42:29 -0700

    added ShermVRevis tweets

--- sampletweets.js
+++ sampletweets.js
@@ -1,20 +1,234 @@
 'use strict';
-
+//search 'from:@RSherman_25 @Revis24'
 {
-  "text": "",
+  "text": "@Revis24 one season u will get 8 picks.... But it won't happen anytime soon... I did it in my 2nd season... So u have something to chase",
   "entities": {
     "user_mentions": [
       {
-        "screen_name": "",
+        "screen_name": "Revis24",
+      }
+    ]
+  },
+  "retweet_count": 1799,
+  "favourites_count": 703,
+  "created_at": "Sat Feb 20 14:07:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@Revis24 don't need ya name... Ur name will be irrelevant once u step back on the field bruh. Get ya picks up!",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      }
+    ]
+  },
+  "retweet_count": 1659,
+  "favourites_count": 549,
+  "created_at": "Sat Feb 20 14:06:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "Remember that time the number of twitter followers mattered more than the production on the field? Me either @Revis24",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      }
+    ]
+  },
+  "retweet_count": 1925,
+  "favourites_count": 1128,
+  "created_at": "Sat Feb 20 15:38:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@Revis24 thank big dog! Much appreciated.",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      }
+    ]
+  },
+  "retweet_count": 387,
+  "favourites_count": 655,
+  "created_at": "Sat Feb 03 10:57:00 +0000 2014",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@dsalisbury62 @incarceratedbob @RealPeterson21 @Revis24 yes be is",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "dsalisbury62",
+      },
+      {
+        "screen_name": "incarceratedbob",
+      },
+      {
+        "screen_name": "RealPeterson21",
       }
     ]
   },
   "retweet_count": 0,
-  "favourites_count" 0,
-  "created_at": "Sun Apr 03 23:48:36 +0000 2011",
+  "favourites_count": 0,
+  "created_at": "Sat Feb 03 10:57:00 +0000 2014",
   "user": {
-    "follower_count": 0,
-    "screen_name": "",
-    "time_zone": "",
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@cocosunday @Revis24 Thank you",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "cocosunday",
+      }
+    ]
+  },
+  "retweet_count": 1,
+  "favourites_count": 6,
+  "created_at": "Sat Sep 29 17:05:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@matt_Condy27 @Revis24 check the stats then talk",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "matt_Condy27",
+      }
+    ]
+  },
+  "retweet_count": 11,
+  "favourites_count": 12,
+  "created_at": "Sat Feb 20 16:19:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@its21blackjack @Revis24 ;) they don't throw to his",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "its21blackjack",
+      }
+    ]
+  },
+  "retweet_count": 5,
+  "favourites_count": 8,
+  "created_at": "Sat Feb 20 17:05:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@rpchudgar @Revis24 he didn't play this year so when he does then he will be in it",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "rpchudgar",
+      }
+    ]
+  },
+  "retweet_count": 8,
+  "favourites_count": 8,
+  "created_at": "Sat Feb 20 09:57:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@DaneCWatson @Revis24 touche",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      },
+      {
+        "screen_name": "DaneCWatson",
+      }
+    ]
+  },
+  "retweet_count": 0,
+  "favourites_count": 0,
+  "created_at": "Sat Jan 24 20:06:00 +0000 2013",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
+  },
+}
+
+{
+  "text": "@Revis24 praying for a fast and full recovery.... Keep ya head up",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "Revis24",
+      }
+    ]
+  },
+  "retweet_count": 2,
+  "favourites_count": 3,
+  "created_at": "Sat Sep 24 09:11:00 +0000 2012",
+  "user": {
+    "follower_count": 1912435,
+    "screen_name": "RSherman_25",
+    "time_zone": "Pacific",
   },
 }
