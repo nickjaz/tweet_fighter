5602a5db89f7e17b7ac59befeba2bea3113e23ac
DKosmos <Davidakosmos@gmail.com>
2017-05-26 13:58:34 -0700

    base tweet obj model

---
+++ sampletweets.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+  "text": "",
+  "entities": {
+    "user_mentions": [
+      {
+        "screen_name": "",
+      }
+    ]
+  },
+  "retweet_count": 0,
+  "favourites_count" 0,
+  "created_at": "Sun Apr 03 23:48:36 +0000 2011",
+  "user": {
+    "follower_count": 0,
+    "screen_name": "",
+    "time_zone": "",
+  },
+}
