f9b4b02b21dacb00db99e43f70ead440c41ef190
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-05-29 13:16:23 -0700

    Added Blout

--- sampletweets.js
+++ sampletweets.js
@@ -389,9 +389,548 @@ var tweets = JSON.stringify([
 },
 },
 
+{
+"text": "@LG_Blount for what? So I can kill the integrity of the defense.... We actually have another good corner who I trust to do his job! We win!",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+  ]
+},
+"retweet_count": 207,
+"favourites_count": 219,
+"created_at": "Sat Mar 12 19:13:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
 
+{
+"text": "@LG_Blount it's all good.... Hard to argue with friendship",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+  ]
+},
+"retweet_count": 203,
+"favourites_count": 194,
+"created_at": "Sat Mar 12 19:13:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
 
+{
+"text": "@JakeParmley_26 @LG_Blount hahaha that's why I am an all pro and SB champ",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+      "screen_name": "JakeParmley",
+    }
+  ]
+},
+"retweet_count": 122,
+"favourites_count": 182,
+"created_at": "Sat Mar 12 19:09:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
 
+{
+"text": "@LG_Blount hahaha I'm just messing with u man.... Bored",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+      "screen_name": "JakeParmley",
+    }
+  ]
+},
+"retweet_count": 19,
+"favourites_count": 53,
+"created_at": "Sat Mar 11 19:44:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@AustinCampos12 @LG_Blount just opinions no numbers as always",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+      "screen_name": "AustinCampos12",
+    }
+  ]
+},
+"retweet_count": 30,
+"favourites_count": 47,
+"created_at": "Sat Mar 11 19:01:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@AustinCampos12 @LG_Blount hahahaha",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "LG_Blount",
+    }
+      "screen_name": "AustinCampos12",
+    }
+  ]
+},
+"retweet_count": 9,
+"favourites_count": 32,
+"created_at": "Sat Mar 11 18:59:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 that one you lock down isn't always the best receiver on the team though... Follow the best receiver every game and do it...",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 437,
+"favourites_count": 238,
+"created_at": "Sat Mar 12 19:11:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 that one you lock down isn't always the best receiver on the team though... Follow the best receiver every game and do it...",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 437,
+"favourites_count": 238,
+"created_at": "Sat Mar 12 19:11:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 I've seen you do it & he has to. But he do it on both sides. He follow the best receiver on the field every game and strap him.",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 262,
+"favourites_count": 131,
+"created_at": "Sat Mar 12 18:52:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 in my opinion he the best bruh sorry... Can't change my mind...",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 305,
+"favourites_count": 268,
+"created_at": "Sat Mar 12 21:44:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 He's the best bruh. I've seen what he does to WR/TE in person and I'm sure you have too. Friendship aside he's the best...",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 276,
+"favourites_count": 199,
+"created_at": "Sat Mar 12 13:19:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 who's the other good corner? We aren't even talking about whoever that is. It doesn't matter. We talking about the best Talib!!",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+  ]
+},
+"retweet_count": 146,
+"favourites_count": 153,
+"created_at": "Sat Mar 12 19:30:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "“@Chezsports: @LG_Blount @KurtMcDonough @RSherman_25 Smith caught four passes” thank you!",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "Chezsports",
+    }
+    "screen_name": "KurtMcDonough",
+    }
+    "screen_name": "LG_Blount",
+    }
+  ]
+},
+"retweet_count": 146,
+"favourites_count": 153,
+"created_at": "Sat Mar 12 19:30:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@LaMichaelJames Aqib the coldest bruh and @RSherman_25  knows that",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "LaMichaelJames",
+    }
+  ]
+},
+"retweet_count": 75,
+"favourites_count": 49,
+"created_at": "Sat Mar 12 19:40:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "“@KurtMcDonough: @RSherman_25 @LG_Blount yo let's not forget Talib during that panthers game, Steve smith did work.” How many yards?",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "KurtMcDonough",
+    }
+  ]
+},
+"retweet_count": 14,
+"favourites_count": 41,
+"created_at": "Sat Mar 12 19:40:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@LaMichaelJames Aqib the coldest bruh and @RSherman_25  knows that",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "LaMichaelJames",
+    }
+  ]
+},
+"retweet_count": 75,
+"favourites_count": 49,
+"created_at": "Sat Mar 12 19:40:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 @AustinCampos12 Julio Jones & Jimmy Graham.... #STRAPPED",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "AustinCampos12",
+    }
+  ]
+},
+"retweet_count": 53,
+"favourites_count": 81,
+"created_at": "Sat Mar 12 19:18:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@RSherman_25 @JakeParmley_26 was Aqib not an all pro/pro bowler this year? I think he was... That's why he got paid like the best... He is..",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "JakeParmley_26",
+    }
+  ]
+},
+"retweet_count": 56,
+"favourites_count": 44,
+"created_at": "Sat Mar 12 19:13:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "“@nathanthorne69: @LG_Blount @Tmoney92080 what you smoking blount ahah die hard pats fan Talib got nothing on @RSherman_25” opinions vary 😒😒",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "RSherman_25",
+    }
+    "screen_name": "nathanthorne69",
+    }
+    "screen_name": "LG_Blount",
+    }
+    "screen_name": "Tmoney92080",
+    }
+  ]
+},
+"retweet_count": 9,
+"favourites_count": 41,
+"created_at": "Sat Mar 12 21:11:00 -7000 2014",
+"user": {
+  "follower_count": 191592,
+  "screen_name": "LG_Blount",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 let me know when u ever have an 8 pick season.... Please....I'll be awaiting that call",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 2782,
+"favourites_count": 1500,
+"created_at": "Sat Mar 13 18:24:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 been tough on u huh lol",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 879,
+"favourites_count": 551,
+"created_at": "Sat Mar 13 18:13:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 is that all u had that season?",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 1938,
+"favourites_count": 1801,
+"created_at": "Sat Mar 13 18:28:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 I checked ya film! It was rough.... Have some pride bout ya play",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 1457,
+"favourites_count": 755,
+"created_at": "Sat Mar 13 18:16:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 I just see a lot of Ls",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 1575,
+"favourites_count": 1169,
+"created_at": "Sat Mar 13 18:17:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 we all do.  Just make sure it's a rare occurrence ....",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 1575,
+"favourites_count": 1169,
+"created_at": "Sat Mar 13 16:44:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@Realrclark25 @DeAngeloHall23 I'm the best at what every coach wants u to do.... Catch a lot of ints and not give up Tds...try it",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 823,
+"favourites_count": 696,
+"created_at": "Sat Mar 13 16:44:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
+
+{
+"text": "@DeAngeloHall23 preciate that",
+"entities": {
+  "user_mentions": [
+    {
+      "screen_name": "@DeAngeloHall23",
+    }
+  ]
+},
+"retweet_count": 36,
+"favourites_count": 78,
+"created_at": "Sat Mar 13 15:54:00 -7000 2014",
+"user": {
+  "follower_count": 1912435,
+  "screen_name": "RSherman_25",
+  "time_zone": "Pacific",
+},
+},
 
 ]
 );
