60122dc5fd5ab97b85a9d94a995627ded7e86b4d
Nick Asmussen <nick.asmussen1@gmail.com>
2017-05-29 16:04:13 -0700

    built list and some rendering

--- render.js
+++ render.js
@@ -1,9 +1,12 @@
 
 var display = document.getElementById('display');
 var form = document.getElementById('form');
-var button = document.getElementById('btn');
-// var twitOne = document.getElementById('twitOne');
-// var twitTwo = document.getElementById('twitTwo');
+var button = document.getElementById('toggler');
+var inputTwitOne = document.getElementById('twitOne');
+var inputTwitTwo = document.getElementById('twitTwo');
+var resultsOne = document.getElementById('results_one');
+var resultsTwo = document.getElementById('results_two');
+var resultsKey = document.getElementById('results_key');
 var twitOne = '';
 var twitTwo = '';
 var twitOneObj;
@@ -39,6 +42,60 @@ function getTweetValues() {
 }
 
 function getWarScore() {
-  twitOneObj.favourites + twitOneObj.reTweets = twitOneObj.warScore;
-  twitTwoObj.favourites + twitTwoObj.reTweets = twitTwoObj.warScore;
+  twitOneObj.warScore = twitOneObj.favourites + twitOneObj.reTweets;
+  twitTwoObj.warScore = twitTwoObj.favourites + twitTwoObj.reTweets;
+}
+
+function renderResults() {
+  form.removeChild(button);
+  display.setAttribute('class','fight');
+  inputTwitOne.setAttribute('class', 'active');
+  inputTwitTwo.setAttribute('class', 'active');
+  inputTwitOne.disable = true;
+  inputTwitOne.disable = true;
+  resultsOne.setAttribute('class', 'show');
+  resultsTwo.setAttribute('class', 'show');
+  resultsKey.setAttribute('class', 'key_show');
+  renderAnimate();
+}
+
+function renderAnimate() {
+  var listOne = document.getElementById('list_one');
+  var listTwo = document.getElementById('list_two');
+  // var listKey = document.getElementById('list_key');
+
+  var favouritesOne = document.createElement('li');
+  var favouritesTwo = document.createElement('li');
+
+  favouritesOne.innerHTML = twitOneObj.favourites;
+  favouritesTwo.innerHTML = twitTwoObj.favourites;
+
+  listOne.appendChild(favouritesOne);
+  listTwo.appendChild(favouritesTwo);
+
+  if (twitOneObj.favourites > twitTwoObj.favourites) {
+    favouritesOne.setAttribute('class', 'winner');
+    favouritesTwo.setAttribute('class', 'loser');
+  } else {
+    favouritesOne.setAttribute('class', 'loser');
+    favouritesTwo.setAttribute('class', 'winner');
+  }
+
+  var reTweetsOne = document.createElement('li');
+  var reTweetsTwo = document.createElement('li');
+
+  reTweetsOne.innerHTML = twitOneObj.reTweets;
+  reTweetsTwo.innerHTML = twitTwoObj.reTweets;
+
+  listOne.appendChild(reTweetsOne);
+  listTwo.appendChild(reTweetsTwo);
+
+  if (twitOneObj.reTweets > twitTwoObj.reTweets) {
+    reTweetsOne.setAttribute('class', 'winner');
+    reTweetsTwo.setAttribute('class', 'loser');
+  } else {
+    reTweetsOne.setAttribute('class', 'loser');
+    reTweetsTwo.setAttribute('class', 'winner');
+  }
+
 }
