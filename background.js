708f1ed0cc38ffa7284d480a0d914042f93d5da8
Nick Asmussen <nick.asmussen1@gmail.com>
2017-06-02 10:22:08 -0700

    background fix

--- background.js
+++ background.js
@@ -3,12 +3,12 @@ function ChangeIt()
 {
   var num = Math.ceil( Math.random() * totalCount );
   //grabs random image from source directory as opposed to an array
-  document.body.background = 'TFassets/'+num+'.jpg';
+  document.body.background = './TFassets/'+num+'.jpg';
 // Background norepeat
   document.body.style.backgroundRepeat = 'no-repeat';
   //align image
   document.body.style.backgroundPosition ='top center';
-  document.body.style.backgroundSize ='top center';
+  document.body.style.backgroundSize ='cover';
 }
 // this line removes linter error
 ChangeIt();
